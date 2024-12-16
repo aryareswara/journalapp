b506260ddaa8ee1f3ca5ad7c808f55f9061f2a4e
Jlesliek24 <joshua.leslie@student.umn.ac.id>
2024-12-16 21:04:27 +0700

    Minor Update on build.gradle.kts App level

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("androidx.navigation.safeargs") version "2.5.2" apply false
+    id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
+    id("com.google.firebase.crashlytics") version "2.9.9" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
